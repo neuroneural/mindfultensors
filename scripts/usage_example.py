b181c9cf20d5e2f1b9cdeb29294e37f8670db5b9
Sergey Plis <s.m.plis@gmail.com>
2024-02-18 22:50:44 -0500

    made it look like a function for volume extraction

--- scripts/usage_example.py
+++ scripts/usage_example.py
@@ -15,45 +15,55 @@ device_name = "cuda:0" if torch.cuda.is_available() else "cpu"
 device = torch.device(device_name)
 
 # dspecifying the database location and collection name
-LABELNOW="50label"
+LABELFIELD = "volumes104"
+DATAFIELD = "T1"
+n_classes = 104
+
 MONGOHOST = "trendscn018.rs.gsu.edu"
-DBNAME = 'MindfulTensors'
-COLLECTION = "HCP"
+DBNAME = "MindfulTensors"
+COLLECTION = "MRN"  # Mindboggle and HCPnew are also in this new format
 # index field and labels to retrieve
-INDEX_ID = "subject"
-VIEWFIELDS = ["subdata", LABELNOW, "id", "subject"]
-SAMPLES = 16 # subcubes per subject to sample
+INDEX_ID = "id"
+
+SAMPLES = 16  # subcubes per subject to sample
 # percent of the data in a collection to use for validation
 validation_percent = 0.1
 
 # specify dimension of the larger volume
-volume_shape = [256]*3
+volume_shape = [256] * 3
 # specify dimension of the subvolume
-subvolume_shape = [256]*3
+subvolume_shape = [256] * 3
 coord_generator = CoordsGenerator(volume_shape, subvolume_shape)
+
+
+def unit_interval_normalize(img):
+    """Unit interval preprocessing"""
+    img = (img - img.min()) / (img.max() - img.min())
+    return img
+
 
 # wrapper functions
 def createclient(x):
-    return create_client(x,
-                         dbname=DBNAME,
-                         colname=COLLECTION,
-                         mongohost=MONGOHOST)
+    return create_client(
+        x, dbname=DBNAME, colname=COLLECTION, mongohost=MONGOHOST
+    )
+
 
 def mycollate_full(x):
-    return mcollate(x, labelname=LABELNOW)
+    return mcollate(x)
+
 
 def mycollate(x):
-    return collate_subcubes(x, coord_generator,
-                            labelname=LABELNOW,
-                            samples=SAMPLES)
+    return collate_subcubes(x, coord_generator, samples=SAMPLES)
+
 
 def mytransform(x):
-    return mtransform(x, label=LABELNOW)
+    return mtransform(x)
 
 
 client = MongoClient("mongodb://" + MONGOHOST + ":27017")
 db = client[DBNAME]
-posts = db[COLLECTION]
+posts = db[COLLECTION + ".meta"]
 # compute how many unique INDEX_ID values are present in the collection
 # these are unique subjects
 num_examples = int(posts.find_one(sort=[(INDEX_ID, -1)])[INDEX_ID] + 1)
@@ -62,9 +72,10 @@ tdataset = MongoDataset(
     range(int((1 - validation_percent) * num_examples)),
     mytransform,
     None,
+    (DATAFIELD, LABELFIELD),
+    normalize=unit_interval_normalize,
     id=INDEX_ID,
-    fields=VIEWFIELDS,
-    )
+)
 
 # We need a sampler that generates indices instead of trying to split the
 # dataset into chunks
@@ -77,7 +88,7 @@ tdataloader = DataLoader(
     sampler=tsampler,
     collate_fn=mycollate_full,
     # if you want the loader to place batch on GPU and at a fixed location
-    #pin_memory=True,
+    # pin_memory=True,
     worker_init_fn=createclient,
-    num_workers=1, # currently does not work with <1
-    )
+    num_workers=1,  # currently does not work with <1
+)
