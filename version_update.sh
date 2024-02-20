73b25f0f6efdd6545219214a3c2d782525b2604e
Girish <79399756+Girish-Anadv-07@users.noreply.github.com>
2024-02-19 20:07:52 -0500

    Update version_update.sh

--- version_update.sh
+++ version_update.sh
@@ -9,7 +9,7 @@ echo $updated_version > version.txt
 git add version.txt version_update.py
 git commit -m "version update to $updated_version"
 git push origin version
-git checkout master
+git checkout main
 
 sed -i -e "1s/__version_info__ = .*/__version_info__ = $updated_version/" version.py
 rm *.py-e || true
