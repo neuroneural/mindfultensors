a41179826c388d78b328b52f92b1f0838d3da901
entodi <eidos92@gmail.com>
2024-02-29 00:46:24 +0000

    Refactor functions from class

--- mindfultensors/creator/mongo_creator/mongo_creator.py
+++ mindfultensors/creator/mongo_creator/mongo_creator.py
@@ -1,19 +1,13 @@
-import bson
-import io
-import numpy as np
-import os
 import pandas as pd
 from typing import Dict, List, Callable, Optional, Any, Tuple
-from tqdm import tqdm
 
-import nibabel as nib
 from pymongo import MongoClient, ASCENDING
-import torch
-from torch import Tensor
 
 
 from mindfultensors.utils import unit_interval_normalize as normalize
 from mindfultensors.creator.base_db_creator import BaseDBCreator
+
+from utils import insert_samples
 
 
 class MongoDBCreator(BaseDBCreator):
@@ -85,12 +79,16 @@ class MongoDBCreator(BaseDBCreator):
         Returns:
         None
         """
-        self._insert_samples(
+        insert_samples(
             data, 
             input_columns,
             label_columns,
             meta_columns,
-            label_description=label_description
+            self._collection_bin,
+            self._collection_meta,
+            label_description=label_description,
+            chunk_size=self._chunk_size,
+            preprocessing_functions=self._preprocessing_functions
         )
         index_name_meta = self._collection_meta.create_index([("id", ASCENDING)])
         index_name_bin = self._collection_bin.create_index([("id", ASCENDING)])
@@ -119,136 +117,6 @@ class MongoDBCreator(BaseDBCreator):
         None
         """
         self._client.close()
-
-    def _insert_samples(
-        self,
-        data: pd.DataFrame,
-        input_columns: List[str],
-        label_columns: List[str],
-        meta_columns: List[str],
-        label_description: Optional[Dict[str, str]] = None,
-    ) -> None:
-        """
-        Insert samples
-
-        Args:
-        data: pd.DataFrame: data
-        label_description: Optional[Dict[str, str]]: dictionary of label description
-
-        Returns:
-        None
-        """
-        selected_columns = input_columns + label_columns + meta_columns
-        for index in tqdm(data.index):
-            meta_data = {"id": index, "labels": {}}
-            for column in selected_columns:
-                shape = None
-                value = data[column].iloc[index]
-                if column in meta_columns:
-                    meta_data[column] = str(value)
-                else:
-                    shape = self._insert_data(column, value, index)
-                    if "shape" not in meta_data:
-                        meta_data["shape"] = shape
-                    else:
-                        assert meta_data["shape"] == shape
-                    if column in label_columns:
-                        if column in label_description:
-                            meta_data["labels"][
-                                column] = label_description[column]
-                        else:
-                            meta_data["labels"][
-                                column] = "Label is not described"
-            self._collection_meta.insert_one(meta_data)
-
-    def _insert_data(
-        self,
-        column: str,
-        filename: str,
-        index: int
-    ) -> Tuple[int]:
-        """
-        Insert data
-
-        Args:
-        column: str: column
-        filename: str: filename
-        index: int: index
-
-        Returns:
-        shape: Tuple[int]: shape
-        """
-        tensor_data = self._data_filename_2_tensor(filename)
-        shape = tensor_data.shape
-        if column in self._preprocessing_functions:
-            tensor_data = self._preprocessing_functions[column](tensor_data)
-        tensor_data = self._tensor_2_bin(tensor_data)
-        # write data
-        for chunk in self._chunk_binobj(tensor_data, index, column, self._chunk_size):
-            self._collection_bin.insert_one(chunk)
-        return shape
-
-    @staticmethod
-    def _data_filename_2_tensor(filename: str) -> Tensor:
-        assert os.path.exists(filename)
-        return torch.from_numpy(np.asanyarray(nib.load(filename).dataobj))
-
-    @staticmethod
-    def _tensor_2_bin(tensor: Tensor) -> bytes:
-        """
-        Convert tensor to binary
-
-        Args:
-        tensor: Tensor: tensor
-
-        Returns:
-        tensor_binary: binary
-        """
-        tensor_1d = tensor.to(torch.uint8)
-        # Serialize tensor and get binary
-        buffer = io.BytesIO()
-        torch.save(tensor_1d, buffer)
-        tensor_binary = buffer.getvalue()
-        return tensor_binary
-
-    @staticmethod
-    def _chunk_binobj(
-        tensor_compressed: Tensor,
-        id: int,
-        kind: str,
-        chunksize: int
-    ) -> Dict[str, Any]:
-        """
-        Chunk the binary object
-
-        Args:
-        tensor_compressed: Tensor: compressed tensor
-        id: int: id
-        kind: str: kind
-        chunksize: int: chunk size
-
-        Returns:
-        Dict[str, Any]: dictionary of chunk
-        """
-        # Convert chunksize from megabytes to bytes
-        chunksize_bytes = chunksize * 1024 * 1024
-
-        # Calculate the number of chunks
-        num_chunks = len(tensor_compressed) // chunksize_bytes
-        if len(tensor_compressed) % chunksize_bytes != 0:
-            num_chunks += 1
-
-        # Yield chunks
-        for i in range(num_chunks):
-            start = i * chunksize_bytes
-            end = min((i + 1) * chunksize_bytes, len(tensor_compressed))
-            chunk = tensor_compressed[start:end]
-            yield {
-                "id": id,
-                "chunk_id": i,
-                "kind": kind,
-                "chunk": bson.Binary(chunk),
-            }
 
 
 if __name__ == "__main__":
